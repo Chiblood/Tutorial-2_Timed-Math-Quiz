7653f815f9415df68a500202e29ba44b5ead69f0
Chiblood <chi.blood6@gmail.com>
2025-09-09 16:18:26 -0400

    Step 2 Complete

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,71 @@ namespace Tutorial_2_Timed_Math_Quiz
 {
     public partial class Form1 : Form
     {
+        // Create a Random Object called randomizer to generate random numbers.
+        Random randomizer = new Random();
+        
+        int addend1;
+        int addend2;
+        int minuend;
+        int subtrahend;
+        int multiplicand;
+        int multiplier;
+        int dividend;
+        int divisor;
+
+        // This method starts the quiz by filling in all of the problems.
+        public void StartTheQuiz()
+        {
+            // Fill in the addition problem.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            // Convert the two randomly generated numbers so they can be displayed in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            // 'sum' is the name of the NumericUpDown control. Ensure that its value is zero before starting the quiz.
+            sum.Value = 0;
+
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            // Start the timer.
+
+        }
+        // These integer variables store the numbers for the addition problem.
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
